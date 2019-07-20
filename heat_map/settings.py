429a3fd6f7c7fbd841594d7b99be6762b4e1df5c
Evan Borchert <eborche2@asu.edu>
2019-07-19 22:43:12 -0400

    Updated to hide accesstoken

--- heat_map/settings.py
+++ heat_map/settings.py
@@ -122,3 +122,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = 'static/'
+
+ACCESSTOKEN = env('ACCESSTOKEN', default="")
--- heat_map/view.py
+++ heat_map/view.py
@@ -1,8 +1,9 @@
 from django.views.generic import View
 from django.shortcuts import render
+from django.conf import settings
 
 
 class Index(View):
 
     def get(self, request):
-        return render(request, 'index.html')
+        return render(request, 'index.html', {'accessToken': settings.ACCESSTOKEN})
